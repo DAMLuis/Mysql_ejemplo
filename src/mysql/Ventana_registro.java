45d25a0b78a3a0273e994d8b428f949ea701b829
DAMLuis <luismdam@gmail.com>
2016-12-28 13:38:52 +0100

    table

--- src/mysql/Ventana_registro.java
+++ src/mysql/Ventana_registro.java
@@ -31,11 +31,9 @@ public class Ventana_registro extends javax.swing.JFrame {
     private void initComponents() {
 
         btn_registro = new javax.swing.JButton();
-        edt_id = new javax.swing.JTextField();
         edt_nombre = new javax.swing.JTextField();
         edt_apellidos = new javax.swing.JTextField();
         edt_edad = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
@@ -51,13 +49,9 @@ public class Ventana_registro extends javax.swing.JFrame {
             }
         });
 
-        edt_id.setToolTipText("ID");
-
         edt_nombre.setToolTipText("Nombre");
 
         edt_edad.setToolTipText("Edad");
-
-        jLabel1.setText("ID");
 
         jLabel2.setText("Nombre");
 
@@ -78,16 +72,13 @@ public class Ventana_registro extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(29, 29, 29)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1)
                             .addComponent(jLabel2)
                             .addComponent(jLabel3)
                             .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(edt_apellidos, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(edt_nombre, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
-                                .addComponent(edt_id))
+                            .addComponent(edt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(edt_edad, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(43, 43, 43)
@@ -102,11 +93,7 @@ public class Ventana_registro extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(61, 61, 61)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(edt_id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(92, 92, 92)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(edt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
@@ -120,7 +107,7 @@ public class Ventana_registro extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(jLabel5)
                     .addComponent(combo_sexo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
                 .addComponent(btn_registro)
                 .addGap(30, 30, 30))
         );
@@ -131,15 +118,15 @@ public class Ventana_registro extends javax.swing.JFrame {
     private void btn_registroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_registroActionPerformed
         try {
             // TODO add your handling code here:
-            
+
             Mysql db = new Mysql();
-            db.MySQLConnection("root", "root", "MySQL_Test");
-            db.insertData("Registros", edt_id.getText(),
+            db.mConnection();
+            db.insertData("Registros",
                     edt_nombre.getText(),
                     edt_apellidos.getText(),
                     edt_edad.getText(),
                     combo_sexo.getSelectedItem().toString());
-            db.closeConnection();
+            //db.closeConnection();
             this.dispose();
         } catch (Exception ex) {
             Logger.getLogger(Ventana_registro.class.getName()).log(Level.SEVERE, null, ex);
@@ -186,9 +173,7 @@ public class Ventana_registro extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> combo_sexo;
     private javax.swing.JTextField edt_apellidos;
     private javax.swing.JTextField edt_edad;
-    private javax.swing.JTextField edt_id;
     private javax.swing.JTextField edt_nombre;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
