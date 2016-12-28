012d513268c56eb5ecda5d6b832dad07bbac5632
DAMLuis <luismdam@gmail.com>
2016-12-28 13:21:47 +0100

    table

--- src/mysql/Mysql.java
+++ src/mysql/Mysql.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -21,11 +22,12 @@ import javax.swing.JOptionPane;
 public class Mysql {
 
     private static Connection Conexion;
+    private  Statement st;
 
-    public void MySQLConnection(String user, String pass, String db_name) throws Exception {
+    public void mConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Conexion = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+            Conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "MySQL_Test", "root", "root");
             JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(Mysql.class.getName()).log(Level.SEVERE, null, ex);
@@ -48,7 +50,7 @@ public class Mysql {
             st.executeUpdate(Query);
             closeConnection();
             try {
-                MySQLConnection("root", "root", name);
+              mConnection();
             } catch (Exception ex) {
                 Logger.getLogger(Mysql.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -72,14 +74,13 @@ public class Mysql {
         }
     }
 
-    public void insertData(String table_name, String ID, String name, String lastname, String age, String gender) {
+    public void insertData(String table_name, String nombre, String apellido, String edad, String sexo) {
         try {
-            String Query = "INSERT INTO " + table_name + " VALUES("
-                    + "\"" + ID + "\", "
-                    + "\"" + name + "\", "
-                    + "\"" + lastname + "\", "
-                    + "\"" + age + "\", "
-                    + "\"" + gender + "\")";
+            String Query = "INSERT INTO " + table_name + "(nombre,apellido,edad,sexo) VALUES("
+                    + "\"" + nombre + "\", "
+                    + "\"" + apellido + "\", "
+                    + "\"" + edad + "\", "
+                    + "\"" + sexo + "\")";
             Statement st = Conexion.createStatement();
             st.executeUpdate(Query);
             JOptionPane.showMessageDialog(null, "Datos almacenados de forma exitosa");
@@ -87,35 +88,34 @@ public class Mysql {
             JOptionPane.showMessageDialog(null, "Error en el almacenamiento de datos");
         }
     }
-    
-    
-    
-    public void getValues(String table_name) {
+
+    public ResultSet getValues(String table_name) {
+          ResultSet resultSet=null;
         try {
             String Query = "SELECT * FROM " + table_name;
-            Statement st = Conexion.createStatement();
-            ResultSet resultSet;
+            st = Conexion.createStatement();
             resultSet = st.executeQuery(Query);
- 
+
             while (resultSet.next()) {
                 System.out.println("ID: " + resultSet.getString("ID") + " "
                         + "Nombre: " + resultSet.getString("Nombre") + " " + resultSet.getString("Apellido") + " "
                         + "Edad: " + resultSet.getString("Edad") + " "
                         + "Sexo: " + resultSet.getString("Sexo"));
             }
- 
+            
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en la adquisición de datos");
         }
+       
+         return resultSet;
     }
 
-    
     public void deleteRecord(String table_name, String ID) {
         try {
             String Query = "DELETE FROM " + table_name + " WHERE ID = \"" + ID + "\"";
             Statement st = Conexion.createStatement();
             st.executeUpdate(Query);
- 
+
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
             JOptionPane.showMessageDialog(null, "Error borrando el registro especificado");
@@ -123,4 +123,43 @@ public class Mysql {
     }
     
     
+    
+
+    public void verTodo(DefaultTableModel tableModel){
+
+        ResultSet resultado = null;
+        tableModel.setRowCount(0);
+        tableModel.setColumnCount(0);
+        String sql = "Registros";
+        try {
+            resultado = getValues(sql);
+            if (resultado != null) {
+                int numeroColumna = resultado.getMetaData().getColumnCount();
+                for (int j = 1; j <= numeroColumna; j++) {
+                    tableModel.addColumn(resultado.getMetaData().getColumnName(j));
+                }
+                while (resultado.next()) {
+                    Object[] objetos = new Object[numeroColumna];
+                    for (int i = 1; i <= numeroColumna; i++) {
+                        objetos[i - 1] = resultado.getObject(i);
+                    }
+                    tableModel.addRow(objetos);
+                }
+            }
+        } catch (SQLException e) {
+        } finally {
+            try {
+                st.close();
+                closeConnection();
+                if (resultado != null) {
+                    resultado.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+  
+
 }
